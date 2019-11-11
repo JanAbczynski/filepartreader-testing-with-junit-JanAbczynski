a665710363da75538fa60accb300319e4c755733
michalkandybowicz <eejdzent@gmail.com>
2019-11-11 15:35:16 +0100

    dodane troche kodu

--- src/main/java/FilePartAnalizer.java
+++ src/main/java/FilePartAnalizer.java
@@ -1,30 +1,48 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class FilePartAnalizer {
     private FilePartReader filePartReader = new FilePartReader();
     ArrayList < String > list_of_words =  filePartReader.readLine(filePath, 1, 7);
     private static String filePath = "csv/file.csv";
 
+
     public FilePartAnalizer() throws IOException {
     }
 
-    public ArrayList <String> getWordsOrderedAlphabetically() throws IOException {
+    public ArrayList <String> getWordsOrderedAlphabetically(int fromLine, int toLine) throws IOException {
         //    calls FilePartReader.readLines ()
         //    returns the words ordered by alphabetically as an ArrayList
-        return null;
+
+        ArrayList< String > lines = filePartReader.readLine(filePath, fromLine, toLine);
+        lines.sort(String.CASE_INSENSITIVE_ORDER);
+        return lines;
 
     }
-    public ArrayList <String> getWordsContainingSubstring (String subString){
+    public ArrayList <String> getWordsContainingSubstring (String subString, int fromLine, int toLine) throws IOException {
         //    calls FilePartReader.readLines ()
-        //    returns the words which contains the
-        return null;
+        //    returns the words which contains the subString
+
+        ArrayList< String > lines = filePartReader.readLine(filePath, fromLine, toLine);
+        ArrayList< String > arrayList = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++ ){
+            if (lines.get(i).contains(subString)){
+                arrayList.add(lines.get(i));
+            }
+        }
+        return arrayList;
 
     }
-    public ArrayList <String> getStringsWhichPalindromes (){
+    public ArrayList <String> getStringsWhichPalindromes (int fromLine, int toLine) throws IOException {
         //    calls FilePartReader.readLines ()
         //    returns the words from the String which are palindrome
-        return null;
+        ArrayList< String > polindromesList = new ArrayList<>();
+        ArrayList< String > lines = filePartReader.readLine(filePath, fromLine, toLine);
+
+        // sprawdzic polindropmy
+
+        return polindromesList;
     }
 
 }
